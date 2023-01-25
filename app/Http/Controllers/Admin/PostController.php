53e417463e2e18bb6a29409b7e3e2501634af222
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-25 19:22:06 +0100

    tabella

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+        $data = [
+            'posts' => Post::All()
+        ];
+
+        return view('admin.posts.index', $data);
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,26 @@
     {{--Qui inserisco i post--}}
 
     <h1>Pagina per i post</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Descrizione</th>
+          </tr>
+        </thead>
 
+
+        @foreach ($posts as $post)
+        <tbody>
+            <tr>
+                <td>{{$post -> id}}</td>
+                <td>{{$post -> title}}</td>
+                <td>{{$post -> description}}</td>
+            </tr>
+        </tbody>>
+        @endforeach
+    </table>
 
 
 @endsection
